754ff3fd6ebb0afa4efe3a99a999262cfa598ed9
GenAlarcon <genaralarcon7@gmail.com>
2024-04-13 14:08:21 -0400

    13-04

---
+++ ajuas.py
@@ -0,0 +1,28 @@
+print("Ingrese un numero")
+num =int(input())
+if num >0:
+    print("Es un numero positivo, como los sueños")
+else:
+    print("Es un numero negativo, como la vida o no")
+
+#Proximo
+
+import random
+
+print("Ingrese su edad")
+num =random.randrange(1,10,1)
+print(num)
+
+#proximo 
+print("Ingrese su edad")
+num =int(input())
+if num >10 and num <=18:
+    print("Usted paga $1000")
+if num >18 and num <=65:
+ print("Usted paga $2000")
+if num >65:
+   print("Usted paga $1500")
+if num <10:
+   print("Usted no paga")
+
+#proximo
