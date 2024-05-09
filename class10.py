b1e34206c6327c23d3a6d9e5c7337311d1f957c6
GenAlarcon <genaralarcon7@gmail.com>
2024-05-08 20:08:06 -0400

    08.05

---
+++ class10.py
@@ -0,0 +1,14 @@
+#float: decimales
+
+edad=int(input("Su edad es "))
+articulos_comprados=int(input("Articulos comprados: "))
+print((edad>18) and (articulos_comprados>1))
+
+l="hoshi"
+h="h"
+c=0
+for letra in l:
+    if letra==h:
+        c=c+1
+print(c)
+
--- pitagoras.py
+++ pitagoras.py
@@ -1,4 +1,3 @@
-
 import math
 def hipotenusa(cateto1,cateto2):
     result=math.sqrt((cateto1**2)+(cateto2**2))
@@ -19,8 +18,8 @@ print(cateto1(hipotenusa,cateto2))
 
 import math
 def cateto2(hipotenusa,cateto1):
-    result=math.sqrt((hipotenusa**2)-(cateto1**2))
-    return result
+   result=math.sqrt((hipotenusa**2)-(cateto1**2))
+   return result
 print("ingrese dos numeros")
 hipotenusa=int(input())
 cateto1=int(input())
